097386e4ce176a18d742e3434e2d70fa59810efb
aloysiusyoko <aloysiusyoko@gmail.com>
2022-03-24 18:32:04 +0700

    hot reload using air

--- handler/rest/http.go
+++ handler/rest/http.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
+
+	_ "github.com/tuingking/flamingo/docs"
 )
 
 func NewHttpHandler(h RestHandler) http.Handler {
@@ -20,13 +23,19 @@ func NewHttpHandler(h RestHandler) http.Handler {
 		r.Get("/health", h.Health)
 	})
 
+	// swagger
+	mux.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("./doc.json")))
+
 	// Web
-	mux.Get("/", h.Home)
+	mux.Route("/", func(r chi.Router) {
+		r.Get("/", h.Home)
+	})
 
 	// API route
 	mux.Route("/api", func(r chi.Router) {
 		r.Route("/products", func(r chi.Router) {
 			r.Get("/", h.GetAllProducts)
+			r.Post("/bulk", h.BulkCreate)
 		})
 	})
 
